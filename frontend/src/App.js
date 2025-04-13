da6b0b6c85e148f4b78f6dbf8a6989b8e6d17fa9
pusilvija <silvijapupsaite@gmail.com>
2025-04-13 21:20:46 +0300

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Task from './Task/Task.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Task></Task>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Task/Task.jsx
@@ -0,0 +1,11 @@
+import './Task.css'
+
+function Task() {
+    return (
+        <div className="task">
+        task1
+        </div>
+    );
+}
+
+export default Task;
