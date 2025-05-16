ec3420160db38c62164d14b9b074315d8577bdbf
pusilvija <silvijapupsaite@gmail.com>
2025-05-16 13:12:19 +0300

    refactor: import format

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Register.css';
+
 import axiosInstance from '../axiosConfig.js';
+
+import './Register.css';
 
 
 const Register = () => {
