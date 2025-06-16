975f0e3b1357bf2a082888e42643bf48a6536f33
pusilvija <silvijapupsaite@gmail.com>
2025-06-16 17:39:41 +0300

    minor refactor class->clasname

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,11 +6,11 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-container">
-        <img src="./images/your-planner.png" alt="Your Planner Logo" class="landing-logo" />
-        <p class="landing-subtitle">Organize your tasks and boost your productivity</p>
-        <div class="landing-buttons">
-            <a href="/taskboard" class="landing-button">Go to Task Board</a>
-            <a href="/about" class="landing-button">Learn More</a>
+        <img src="./images/your-planner.png" alt="Your Planner Logo" className="landing-logo" />
+        <p className="landing-subtitle">Organize your tasks and boost your productivity</p>
+        <div className="landing-buttons">
+            <a href="/taskboard" className="landing-button">Go to Task Board</a>
+            <a href="/about" className="landing-button">Learn More</a>
         </div>
     </div>
   );
