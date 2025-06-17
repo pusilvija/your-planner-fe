e6c44e80da66488050b971bee2780d78e8f6e075
pusilvija <silvijapupsaite@gmail.com>
2025-06-17 14:58:53 +0300

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; 
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
 import './About.css';
 
 const About = () => {
@@ -10,7 +11,7 @@ const About = () => {
             <p className="about-description">
                 Your Planner is a personal task management app designed to help you organize your tasks, boost your productivity, and stay on top of your goals. With features like a visual task board, drag-and-drop functionality, and detailed task filtering, Your Planner makes managing your tasks simple and efficient.
             </p>
-            <p className="about-features">
+            <div className="about-features">
                 <strong>Key Features:</strong>
                 <ul>
                     <li>User authentication and secure data storage</li>
@@ -19,35 +20,35 @@ const About = () => {
                     <li>Task filtering and sorting</li>
                     <li>Integration with weather data for planning</li>
                 </ul>
-            </p>
+            </div>
 
-            <p className="about-technology">
-            <strong>Tech Stack:</strong>
+            <div className="about-technology">
+                <strong>Tech Stack:</strong>
                 <ul>
                     <li><strong>Django</strong>: Python web framework for building secure and scalable web applications.</li>
-                    <li><strong>Django REST Framework</strong>,: toolkit for building RESTful APIs with Django.</li>
-                    <li><strong>Token Authentication</strong>,: a stateless authentication mechanism using tokens for secure API access.</li>
+                    <li><strong>Django REST Framework</strong>: toolkit for building RESTful APIs with Django.</li>
+                    <li><strong>Token Authentication</strong>: a stateless authentication mechanism using tokens for secure API access.</li>
                     <li><strong>PostgreSQL</strong>: open-source relational database system for storing application data.</li>
                     <li><strong>React</strong>: component-based frontend</li>
-                    <li><strong>React Router</strong>,: routing between views</li>
+                    <li><strong>React Router</strong>: routing between views</li>
                     <li><strong>Axios</strong>: API requests</li>
-                    <li><strong>React</strong> Beautiful DnD: drag & drop functionality</li>
+                    <li><strong>React Beautiful DnD</strong>: drag & drop functionality</li>
                     <li><strong>Docker</strong>: for deploying in Railway</li>
-                    </ul>
-            </p>
+                </ul>
+            </div>
 
-            <p className="whats-next">
-            <strong>What is next?</strong>
-            <ul>
-                <li>Enhancing mobile usability to make the app more convenient for users on phones.</li>
-                <li>Adding more task features, such as created_at and deadline to provide better task tracking.</li>
-                <li>Introducing customizable containers, allowing users to create their own task categories and organize their thoughts freely.</li>
-                <li>Continuously exploring new possibilities to make Your Planner even more versatile and user-friendly. The world is your oyster!</li>
-            </ul>
-            </p>
+            <div className="whats-next">
+                <strong>What is next?</strong>
+                <ul>
+                    <li>Enhancing mobile usability to make the app more convenient for users on phones.</li>
+                    <li>Adding more task features, such as created_at and deadline to provide better task tracking.</li>
+                    <li>Introducing customizable containers, allowing users to create their own task categories and organize their thoughts freely.</li>
+                    <li>Continuously exploring new possibilities to make Your Planner even more versatile and user-friendly. The world is your oyster!</li>
+                </ul>
+            </div>
 
-            <p className="github-links">
-                <strong>Github:</strong>
+            <div className="github-links">
+                <strong>GitHub:</strong>
                 <ul>
                     <li>
                         <a href="https://github.com/pusilvija/your-planner" target="_blank" rel="noopener noreferrer">
@@ -60,9 +61,42 @@ const About = () => {
                         </a>
                     </li>
                 </ul>
-            </p>
+            </div>
+
+            <div className="contact-buttons">
+                {/* Gmail Button */}
+                <a
+                    href="mailto:silvijapupsaite@gmail.com"
+                    className="contact-button email-button"
+                    aria-label="Email"
+                >
+                    <FaEnvelope className="icon" />
+                </a>
+
+                {/* LinkedIn Button */}
+                <a
+                    href="https://www.linkedin.com/in/silvija-pup%C5%A1ait%C4%97-b76742168?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                    className="contact-button linkedin-button"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="LinkedIn"
+                >
+                    <FaLinkedin className="icon" />
+                </a>
+
+                {/* GitHub Button */}
+                <a
+                    href="https://github.com/pusilvija"
+                    className="contact-button github-button"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="GitHub"
+                >
+                    <FaGithub className="icon" />
+                </a>
+            </div>
             <button
-                className="taskdetails-buttons"
+                className="about-buttons"
                 id="back-button"
                 onClick={() => navigate(-1)}
             >
